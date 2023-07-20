1a57b205d1bf0ba1ccfb3ec2ae1ad46ef56059fa
Luis López Ortiz <96153356+LuisLotiz@users.noreply.github.com>
2023-07-20 02:21:41 +0000

    Armado con error en ingreso de icono

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,40 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import Proptypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props){
+    return (<div className="bigCounter">
+    <div className="calendar"> </div>
+        <i className="fa-regular fa-clock"></i>
+    <div className="four">{props.digitFour % 10}</div>
+    <div className="three">{props.digitThree % 10}</div>
+    <div className="two">{props.digitTwo % 10}</div>
+    <div className="one">{props.digitOne % 10}</div>
+    </div>);
+}
+MyComponent.proptypes = {
+    digitFour: Proptypes.number,
+    digitThree: Proptypes.number,
+    digitTwo: Proptypes.number,
+    digitOne: Proptypes.number,
+};
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0;
+setInterval(function(){
+    const four = Math.floor(counter/1000);
+    const three = Math.floor(counter/100);
+    const two = Math.floor(counter/10);
+    const one = Math.floor(counter/1);
+    console.log(four, three, two, one);
+    counter++;
+},1000);
+    //render your react application
+    ReactDOM.render(
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, 
+    document.querySelector("#app")
+    );
+
